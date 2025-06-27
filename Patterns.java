53288710ba6b1e2789c6441cca6a5812d99e6213
Harshita638 <ss7799516@gmail.com>
2025-06-27 18:35:43 +0530

    2nd commit

--- Patterns.java
+++ Patterns.java
@@ -1,12 +1,15 @@
-public static void main(String args[]) {
-    int n = 4;
+public class Patterns {
+    public static void main(String[] args) {
+        int n = 5;
 
-    // outer loop
-    for (int i = 1; i <= n; i++) {
-        // inner loop
-        for (int j = 1; j <= i; j++) {
-            System.out.print("*");
+        // outer loop for rows
+        for (int i = 1; i <= n; i++) {
+            // inner loop for numbers in each row
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            // move to next line after each row
+            System.out.println();
         }
-        System.out.println();
     }
 }
